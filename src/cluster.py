8f3c8d1a38e2178fcbe8606e7fd3439a884ede89
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-05-11 11:15:18 +0200

    Simplified stuff

--- src/cluster.py
+++ src/cluster.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         .filter_customer('7fd935a6-a1f5-42d1-8b5f-048dd54c07d1')\
         .filter_country('SI')
 
-    articles: List[Article] = requests.gets('2023-05-09')
+    articles: List[Article] = requests.gets('2023-05-10T08:00:00', '2023-05-11T08:00:00')
 
     openai_embed(articles, 'openai_embd')
     ttnx_embed(articles, 'ttnx_embd')
