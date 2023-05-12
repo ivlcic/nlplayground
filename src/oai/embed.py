91e755b939edfb35eb7626684367ea4b2d2127b6
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-05-12 11:01:23 +0200

    Simplified stuff

--- src/oai/embed.py
+++ src/oai/embed.py
@@ -9,7 +9,7 @@ from oai.tokenize import truncate_text_tokens
 logger = logging.getLogger('oai.embed')
 
 
-def openai_embed(articles: List[Article], embed_field_name: str):
+def openai_embed(articles: List[Article], embed_field_name: str, cache: bool = True):
     for a in articles:
         if a.from_cache('data'):  # read from file
             if embed_field_name in a.data:  # we already did the embedding ($$$$)
@@ -26,4 +26,5 @@ def openai_embed(articles: List[Article], embed_field_name: str):
         )
         logger.info('Loaded %s article OpenAI embedding.', a)
         a.data[embed_field_name] = embedding["data"][0]["embedding"]  # extract vector from response
-        a.to_cache('data')  # cache article to file
+        if cache:
+            a.to_cache('data')  # cache article to file
