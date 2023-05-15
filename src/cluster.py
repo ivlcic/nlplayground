408b2c565efc6e4281363c94b288accb37e6b4c9
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-05-15 12:58:18 +0200

    Initial commit

--- src/cluster.py
+++ src/cluster.py
@@ -4,7 +4,7 @@ from typing import List
 from kl.articles import Articles, Article
 from oai.embed import openai_embed
 from ttnx.cluster import cluster_louvain, cluster_print, cluster_compare, cluster_ttxn
-from ttnx.constants import TTNX_AVG_SQUEEZE, TTNX_AVG_SENTENCE, TTNX_AVG_TRUNCATE, TTNX_AVG_NONE
+from ttnx.constants import TTNX_AVG_SQUEEZE, TTNX_AVG_SENTENCE, TTNX_AVG_TRUNCATE, TTNX_AVG_NONE, TTNX_WEIGHT_NEG_LIN
 from ttnx.embed import ttnx_embed
 
 if __name__ == "__main__":
@@ -21,11 +21,12 @@ if __name__ == "__main__":
 
     openai_embed(articles, 'openai_embd')
     # ttnx_embed(articles, 'ttnx_embd', cache=False, average_t=TTNX_AVG_NONE)  # old mode
-    # ttnx_embed(articles, 'ttnx_embd')
+    ttnx_embed(articles, 'ttnx_embd', cache=False, average_t=TTNX_AVG_SQUEEZE, weight_t=TTNX_WEIGHT_NEG_LIN)  # old mode
+    #ttnx_embed(articles, 'ttnx_embd', cache=False)
 
     oai_l_clusters = cluster_louvain(articles, 'openai_embd', 0.92)
-    #ttnx_l_clusters = cluster_louvain(articles, 'ttnx_embd', 0.75)
-    ttnx_l_clusters = cluster_ttxn(articles, 'vector_768___doc_embed___sbert___pmmb-v2-kl-ijs', 0.75)
+    ttnx_l_clusters = cluster_louvain(articles, 'ttnx_embd', 0.84)
+    #ttnx_l_clusters = cluster_ttxn(articles, 'vector_768___doc_embed___sbert___pmmb-v2-kl-ijs', 0.84)
 
     print('')
     print('========================== OpenAI ========================== ')
